4cc7b5c6c219a1ae083f47278497f70f10dd5839
Pierre Verbe <pierre.verbe@free.fr>
2020-09-30 21:08:37 +0200

    Added SampleTest

--- Sbt-Scala-Spark/src/test/scala/com/gitHub/ScalaSpark/sparkTestingBase/SampleTest.scala
+++ Sbt-Scala-Spark/src/test/scala/com/gitHub/ScalaSpark/sparkTestingBase/SampleTest.scala
@@ -1,5 +1,13 @@
 package com.gitHub.ScalaSpark.sparkTestingBase
 
-class SampleTest {
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.funsuite.AnyFunSuite
 
+class SampleTest extends AnyFunSuite with SharedSparkContext{
+  test("test initializing spark context") {
+    val list = List(1, 2, 3, 4)
+    val rdd = sc.parallelize(list)
+
+    assert(rdd.count === list.length)
+  }
 }
